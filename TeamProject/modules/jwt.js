d5d077283f7e700ffbe22beecabadf2bdded1437
qoxogus <a01034367055@gmail.com>
2021-02-20 21:27:43 +0900

    [add jwt]

---
+++ TeamProject/modules/jwt.js
@@ -0,0 +1,42 @@
+const randToken = require('rand-token');
+const jwt = require('jsonwebtoken');
+const secretKey = require('../config/secretkey').secretKey;
+const options = require('../config/secretkey').option;
+const TOKEN_EXPIRED = -3;
+const TOKEN_INVALID = -2;
+
+module.exports = {
+    sign: async (dbemail, ismanager) => {
+        const payload = {
+            email: dbemail,
+            ismanager: ismanager
+        };
+        const result = {
+            //sign메소드를 통해 access token 발급
+            token: jwt.sign(payload, secretKey, options),
+            refreshToken: randToken.uid(256)
+        };
+        console.log(result.token);
+        return result;
+    },
+    verify: async (token) => {
+        let decoded;
+        try {
+            // verify를 통해 값 decode
+            decoded = jwt.verify(token, secretKey);
+        } catch (err) {
+            if (err.message === 'jwt expired') {
+                console.log('expired token');
+                return TOKEN_EXPIRED;
+            } else if (err.message === 'invalid token') {
+                console.log('invalid token');
+                console.log(TOKEN_INVALID);
+                return TOKEN_INVALID;
+            } else {
+                console.log("invalid token");
+                return TOKEN_INVALID;
+            }
+        }
+        return decoded;
+    }
+}
